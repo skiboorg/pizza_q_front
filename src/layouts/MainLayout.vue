ab1ce98a22189e32233b0bb0d2560d3eda350516
skiboorg <d@skib.org>
2021-04-11 22:30:39 +0300

    remove adresses

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -84,43 +84,43 @@
 
 
       </q-list>
-      <div style="display: none" class="q-px-sm">
+<!--      <div  class="q-px-sm">-->
 
-        <p class="text-bold">Ваш город</p>
-        <q-btn-dropdown
-          split
-          color="primary"
-          disable-main-btn
-          class="full-width q-mb-md"
-          push
-          no-caps>
-          <template v-slot:label>
-            <div class="row items-center no-wrap">
-              <q-icon left name="location_city" />
-              <div class="text-center">
-                {{currentCity.name}}
-              </div>
-            </div>
-          </template>
+<!--        <p class="text-bold">Ваш город</p>-->
+<!--        <q-btn-dropdown-->
+<!--          split-->
+<!--          color="primary"-->
+<!--          disable-main-btn-->
+<!--          class="full-width q-mb-md"-->
+<!--          push-->
+<!--          no-caps>-->
+<!--          <template v-slot:label>-->
+<!--            <div class="row items-center no-wrap">-->
+<!--              <q-icon left name="location_city" />-->
+<!--              <div class="text-center">-->
+<!--                {{currentCity.name}}-->
+<!--              </div>-->
+<!--            </div>-->
+<!--          </template>-->
 
-          <q-list>
-            <q-item v-for="city in cities" :key="city.id" clickable v-close-popup @click="changeCity(city.id)">
+<!--          <q-list>-->
+<!--            <q-item v-for="city in cities" :key="city.id" clickable v-close-popup @click="changeCity(city.id)">-->
 
-              <q-item-section>
-                <q-item-label>{{city.name}}</q-item-label>
-                <q-item-label caption v>{{city.info}}</q-item-label>
-              </q-item-section>
-              <q-item-section side v-if="city.id === $q.cookies.get('city_id')">
-                <q-icon name="check" color="positive" />
-              </q-item-section>
-            </q-item>
+<!--              <q-item-section>-->
+<!--                <q-item-label>{{city.name}}</q-item-label>-->
+<!--                <q-item-label caption v>{{city.info}}</q-item-label>-->
+<!--              </q-item-section>-->
+<!--              <q-item-section side v-if="city.id === $q.cookies.get('city_id')">-->
+<!--                <q-icon name="check" color="positive" />-->
+<!--              </q-item-section>-->
+<!--            </q-item>-->
 
 
-          </q-list>
-        </q-btn-dropdown>
-        <p class="q-mb-sm text-bold">{{currentCity.adresses.length>1 ? 'Адреса кафе' : 'Адрес кафе'}}</p>
-        <p class="q-mb-none" v-for="adress in currentCity.adresses" :key="adress.id">{{adress.address}}</p>
-      </div>
+<!--          </q-list>-->
+<!--        </q-btn-dropdown>-->
+<!--        <p class="q-mb-sm text-bold">{{currentCity.adresses.length>1 ? 'Адреса кафе' : 'Адрес кафе'}}</p>-->
+<!--        <p class="q-mb-none" v-for="adress in currentCity.adresses" :key="adress.id">{{adress.address}}</p>-->
+<!--      </div>-->
 
     </q-drawer>
     <q-page-container style="padding-top: 0">
