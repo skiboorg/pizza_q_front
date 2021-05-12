d4f7de04a29d357d667735b055953ff006b9e54e
skiboorg <d@skib.org>
2021-05-12 19:30:02 +0300

    change discount

--- src/pages/vacancy.vue
+++ src/pages/vacancy.vue
@@ -3,7 +3,7 @@
   <div class="container">
      <h1 class="text-h3 f-raleway-900">Вакансии</h1>
     <div class="row items-center q-mb-lg-xl q-mb-md-xl q-mb-sm-xl q-mb-xs-md">
-        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 ">
+        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 q-mb-lg-none q-mb-md-none q-mb-sm-md q-mb-xs-md">
         <p class="q-mb-none"><span class="text-weight-bold">В связи с постоянным развитием и расширением нам требуются сотрудники:</span> <br><br>
 
           <span class="text-weight-bold">Кухонный рабочий</span>, зарплата: от 45 000 руб.<br><br>
@@ -12,15 +12,14 @@
 <span class="text-weight-bold">Шашлычник</span>, зарплата: до 85 000 руб.<br><br>
 
   <span class="text-weight-bold">Условия:</span><br>
-
 - Комплексное питание за наш счет;<br>
 - Развозим сотрудников после смены;<br>
 - Премирование и бонусы. </p>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 offset-lg-1 offset-md-1 offset-sm-1 offset-xs-0">
+        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 offset-lg-1 offset-md-1 offset-sm-0 offset-xs-0">
           <q-img
             cover
-            src="~assets/vac.jpg"
+            src="~assets/vac.webp"
             :ratio="16/9"
             class="rounded-borders"/>
         </div>
