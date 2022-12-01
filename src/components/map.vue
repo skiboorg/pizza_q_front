90f23254a2e61de0610667c8d3685010535c132e
skiboorg <d@skib.org>
2022-12-01 09:43:14 +0300

    add first use promo

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="row q-mb-lg-xl q-mb-md-xl q-mb-sm-xl q-mb-xs-md ">
+
     <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 order-lg-first order-md-first order-sm-first order-xs-last">
         <yandex-map
           :coords="coords"
@@ -21,8 +22,12 @@
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 offset-lg-1 offset-md-1 offset-sm-1 offset-xs-0 order-lg-last order-md-last order-sm-last order-xs-first q-py-xl">
       <h3 class="text-h4 f-raleway-900 q-mb-md">Мы находимся по адресу:</h3>
 
-      <p v-if="currentCity.id===1" @click="coords=[66.088411, 76.662433],selectedAddress=1"  :class="{'link-active':selectedAddress===1}" class="text-decoration-dash cursor-pointer">Новый Уренгой, ул.Железнодорожная д.12</p>
-      <p v-if="currentCity.id===2" @click="coords=[57.194362, 65.585954],selectedAddress=1"  :class="{'link-active':selectedAddress===1}" class="text-decoration-dash cursor-pointer">Тюмень, Щербакова, 158/25</p>
+      <p v-if="currentCity.id===1" @click="coords=[66.088411, 76.662433],selectedAddress=1"
+         :class="{'link-active':selectedAddress===1}" class="text-decoration-dash cursor-pointer">Новый Уренгой, ул.Железнодорожная д.12</p>
+      <p v-if="currentCity.id===3" @click="coords=[64.910244, 77.781222],selectedAddress=1"
+         :class="{'link-active':selectedAddress===1}" class="text-decoration-dash cursor-pointer">Тарко-Сале, ул.Строителей,19</p>
+       <p v-if="currentCity.id===4" @click="coords=[65.859078, 74.490612],selectedAddress=1"
+         :class="{'link-active':selectedAddress===1}" class="text-decoration-dash cursor-pointer">Пангоды, ул. Звездная, 19</p>
 <!--      <p class="text-h5 text-bold">Другие наши кафе по адресам:</p>-->
 <!--      <p @click="coords=[62.236750, 74.532249],selectedAddress=2" class="text-decoration-dash q-mb-sm cursor-pointer" :class="{'link-active':selectedAddress===2}">Когалым, Кирова 9</p><br>-->
 <!--      <p @click="coords=[63.198711, 75.451836],selectedAddress=3" class="text-decoration-dash q-mb-sm cursor-pointer" :class="{'link-active':selectedAddress===3}">Ноябрьск, Дзержинского 12б</p><br>-->
@@ -31,9 +36,14 @@
       <div class="flex items-center justify-start">
         <img class="q-mr-md" src="~assets/red-phone-icon.png" alt="">
 <q-no-ssr>
-           <p v-if="currentCity.id===1" class="q-mb-sm q-mr-md"><a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73494292407">+7 (3494) 29 24 07</a></p>
-          <p v-if="currentCity.id===1" class="q-mb-sm q-mr-md"><a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73494292507">+7 (3494) 29 25 07</a></p>
-          <p v-if="currentCity.id===2" class="q-mb-sm q-mr-md"><a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73452925577">+7 (3452) 92-55-77</a></p>
+           <p v-if="currentCity.id===1" class="q-mb-sm q-mr-md">
+             <a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73494292407">+7 (3494) 29 24 07</a></p>
+          <p v-if="currentCity.id===1" class="q-mb-sm q-mr-md">
+            <a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73494292507">+7 (3494) 29 25 07</a></p>
+          <p v-if="currentCity.id===3" class="q-mb-sm q-mr-md">
+            <a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73452925577">+7 (908) 863 56 25</a></p>
+  <p v-if="currentCity.id===4" class="q-mb-sm q-mr-md">
+            <a class="no-text-decoration text-grey-10 text-decoration-dash zphone" href="tel:+73452925577">+7 (951) 994-74-40</a></p>
 </q-no-ssr>
 
       </div>
@@ -57,7 +67,17 @@ export default {
   computed:{
     ...mapGetters('city',['currentCity']),
     coords(){
-      return this.currentCity.id===1 ? [66.088411, 76.662433] : [57.194362, 65.585954]
+      let coord
+      if (this.currentCity.id===1){
+        return [66.088411, 76.662433]
+      }
+      if (this.currentCity.id===3){
+        return [64.910244, 77.781222]
+      }
+      if (this.currentCity.id===4){
+        return [65.859078, 74.490612]
+      }
+
     }
 },
 
