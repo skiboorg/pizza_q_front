231b42b1dc437820a0ba5b4bb45b26c63dc469cf
xxx <ddnnss@mail.ru>
2024-05-02 11:49:49 +0300

    pwa

--- src/pages/order/order.vue
+++ src/pages/order/order.vue
@@ -1,8 +1,7 @@
 <template>
   <q-page  class="flex items-center justify-center column">
     <h3 class="text-h4 text-bold text-center">Ваш заказ <span class="text-primary">№ {{$route.params.order_code}}</span> успешно оплачен</h3>
-    <p class="text-bold text-center">Спасибо, за ваш заказ!<br>
-Менеджер свяжется с вами в течении 5 минут, для подтверждения заказа.</p>
+    <p class="text-bold text-center">Спасибо, за ваш заказ!</p>
   <q-btn no-caps unelevated  color="primary" to="/" label="На главную"/>
   </q-page>
 </template>
